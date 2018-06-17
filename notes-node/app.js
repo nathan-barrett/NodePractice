a4720e5e9638c7ef233da640667f9df90e4672d5
Nathan Barrett <demeules.barrett@gmail.com>
2018-06-17 11:25:15 -0700

    work through practice

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,10 +5,10 @@ const os =require('os');
 const _ = require('lodash');
 const notes = require('./notes.js')
 
-let res = notes.add(5, 10);
-console.log(res);
-let user = os.userInfo();
+// let res = notes.add(5, 10);
+// console.log(res);
+// let user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-  if (err) throw err;
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
+//   if (err) throw err;
+// });
